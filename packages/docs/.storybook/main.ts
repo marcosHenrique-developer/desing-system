76ffced5e226d410e92b86ef1b914a5974bf8647
MARCOS <marcos@MacBook-Air-de-MARCOS.local>
2024-01-05 13:28:02 -0300

    publicação e manutenção publicando Storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -25,5 +25,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/desing-system/";
+    }
+
+    return config;
+  },
 };
 export default config;
